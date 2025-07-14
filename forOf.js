800ffaf1d78c5c4239a86cd3e4aa17d52d94c41c
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-14 23:13:36 +0600

    for

--- forOf.js
+++ forOf.js
@@ -1,3 +1,14 @@
 const numbers = [12, 89, 45, 63, 21, 72];
 
 console.log(numbers);
+
+// for(const num of numbers) {
+//   console.log(num);
+// }
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+
+for(const frut of fruits) {
+  console.log(frut);
+  console.log(' I want to eat');
+}
