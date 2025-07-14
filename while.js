d93d9d06d09e642f11a0c203aaaa847d2db37543
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-14 23:31:03 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if(number < 10) {
+  console.log('The number is smaller than 10');
+}
+console.log('something new is cooking');
+
+// --------------------WHILE LOOP-----------------------
+let num = 0;
+while(num < 5) {
+  console.log('looping', num);
+  // num = num + 1;
+  // num += 1;
+  num++;
+}
