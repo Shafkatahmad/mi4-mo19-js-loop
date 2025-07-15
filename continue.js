65982d388107f22326ea6ef0335accb8d3f4444c
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-15 23:31:04 +0600

    continue

---
+++ continue.js
@@ -0,0 +1,9 @@
+// SKIP REST OF THE CODE FOR THIS ITERATION
+// BREAK --> I AM DONE WITH THE LOOP. LOOP END.
+
+for(let i = 1; i < 10; i++) {
+  if(i % 2 !==0) {
+    continue;
+  }
+  console.log(i);
+}
